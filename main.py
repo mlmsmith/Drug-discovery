f412f12af81d31ae3bd85ece6519298b0525fc3e
mlmsmith <mlmsmith@hotmail.co.uk>
2025-03-03 21:29:13 +0000

    added selected_targets

--- main.py
+++ main.py
@@ -5,3 +5,6 @@ target = new_client.target
 target_query = target.search('coronavirus')
 targets = pd.DataFrame.from_dict(target_query)
 targets
+
+selected_target = targets.target_chembl_id[4]
+selected_target
